f79ace05aae43c6ae7f27353365f8b70d0388b4c
Afra Taiza <afrataiza@gmail.com>
2022-05-24 17:51:56 -0300

    fix: correção da branch

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,6 +9,7 @@ class Login extends React.Component {
     this.state = {
       name: '',
       email: '',
+      isRedirect: false,
     };
   }
 
@@ -35,40 +36,54 @@ class Login extends React.Component {
     history.push('/game');
   };
 
+  settingsButton = () => {
+    const { history } = this.props;
+    history.push('/settings');
+  }
+
   render() {
     return (
-      <form>
-        <label htmlFor="name">
-          Nome
-          <input
-            type="text"
-            name="name"
-            id="name"
-            data-testid="input-player-name"
-            onChange={ this.handleChange }
-          />
-        </label>
+      <div>
+        <form>
+          <label htmlFor="name">
+            Nome
+            <input
+              type="text"
+              name="name"
+              id="name"
+              data-testid="input-player-name"
+              onChange={ this.handleChange }
+            />
+          </label>
 
-        <label htmlFor="email">
-          Email
-          <input
-            type="text"
-            name="email"
-            id="email"
-            data-testid="input-gravatar-email"
-            onChange={ this.handleChange }
-          />
-        </label>
+          <label htmlFor="email">
+            Email
+            <input
+              type="text"
+              name="email"
+              id="email"
+              data-testid="input-gravatar-email"
+              onChange={ this.handleChange }
+            />
+          </label>
 
+          <button
+            type="button"
+            data-testid="btn-play"
+            disabled={ !this.enableButton() }
+            onClick={ this.sendNameToGlobalState }
+          >
+            Play
+          </button>
+        </form>
         <button
           type="button"
-          data-testid="btn-play"
-          disabled={ !this.enableButton() }
-          onClick={ this.sendNameToGlobalState }
+          data-testid="btn-settings"
+          onClick={ this.settingsButton }
         >
-          Play
+          Settings
         </button>
-      </form>
+      </div>
     );
   }
 }
