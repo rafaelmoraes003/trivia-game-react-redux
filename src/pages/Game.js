1f986e6be9e7b5700d5521e168f6d59008fdb1f5
Afra Taiza <afrataiza@gmail.com>
2022-05-25 18:54:47 -0300

    feat: requisito 6

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,14 +1,88 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Game extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      trivia: [],
+      index: 0,
+      answers: [],
+    };
+  }
+
+  async componentDidMount() {
+    const token = localStorage.getItem('token');
+    const triviaFetch = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
+    const response = await triviaFetch.json();
+    if (response.response_code > 0) {
+      const { history } = this.props;
+      history.push('/');
+    } else {
+      let answers;
+      const { index } = this.state;
+      if (response.results[index].type === 'multiple') {
+        answers = [
+          { id: 0, answer: response.results[index].incorrect_answers[0] },
+          { id: 1, answer: response.results[index].incorrect_answers[1] },
+          { id: 2, answer: response.results[index].incorrect_answers[2] },
+          { id: 3, answer: response.results[index].correct_answer },
+        ];
+      } else {
+        answers = [
+          { id: 0, answer: response.results[index].incorrect_answers[0] },
+          { id: 3, answer: response.results[index].correct_answer },
+        ];
+      }
+      const n = 0.5;
+      this.setState({ trivia: response.results,
+        answers: answers.sort(() => Math.random() - n) });
+    }
+  }
+
   render() {
+    const { trivia, index, answers } = this.state;
     return (
       <div data-testid="game-div">
         <Header />
+        {trivia.length > 0 && (
+          <div>
+            <p data-testid="question-category">{ trivia[index].category}</p>
+            <p data-testid="question-text">{trivia[index].question}</p>
+            <div data-testid="answer-options">
+              {answers.map(({ id, answer }) => {
+                if (id === 2 + 1) {
+                  return (
+                    <button
+                      key={ id }
+                      type="button"
+                      data-testid="correct-answer"
+                    >
+                      {answer}
+                    </button>
+                  );
+                } return (
+                  <button
+                    key={ id }
+                    type="button"
+                    data-testid={ `wrong-answer-${id}` }
+                  >
+                    {answer}
+                  </button>
+                );
+              })}
+            </div>
+          </div>
+        )}
       </div>
     );
   }
 }
 
+Game.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 export default Game;
