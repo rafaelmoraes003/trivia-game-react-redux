219c64f96fd0fef8d86b3a31d90ed9615f0f6dfc
rafael <rafaelsm003@gmail.com>
2022-05-26 17:41:10 -0300

    feat: requisito 8

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -10,36 +10,52 @@ class Game extends React.Component {
       index: 0,
       answers: [],
       showAnswers: false,
+      timer: 10,
+      disabled: false,
     };
   }
 
-  async componentDidMount() {
+  componentDidMount() {
+    this.fetchTrivia();
+    const MAX_SECONDS = 1000;
+
+    setInterval(() => {
+      const { trivia, timer, showAnswers } = this.state;
+      if (timer > 0) {
+        this.setState((prevState) => ({
+          timer: prevState.timer - 1,
+          disabled: false,
+        }));
+      } else if (timer === 0 && !showAnswers) {
+        this.setState({
+          disabled: true,
+          showAnswers: false,
+          timer: 0,
+        });
+      } else if (timer === 0 && showAnswers) {
+        this.setState((prevState) => ({
+          timer: 10,
+          index: (prevState.index + 1) % trivia.length,
+          showAnswers: false,
+        }));
+      }
+    }, MAX_SECONDS);
+  }
+
+  fetchTrivia = async () => {
     const token = localStorage.getItem('token');
     const triviaFetch = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
     const response = await triviaFetch.json();
     if (response.response_code > 0) {
       const { history } = this.props;
       history.push('/');
-    } else {
-      let answers;
-      const { index } = this.state;
-      if (response.results[index].type === 'multiple') {
-        answers = [
-          { id: 0, answer: response.results[index].incorrect_answers[0] },
-          { id: 1, answer: response.results[index].incorrect_answers[1] },
-          { id: 2, answer: response.results[index].incorrect_answers[2] },
-          { id: 3, answer: response.results[index].correct_answer },
-        ];
-      } else {
-        answers = [
-          { id: 0, answer: response.results[index].incorrect_answers[0] },
-          { id: 3, answer: response.results[index].correct_answer },
-        ];
-      }
-      const n = 0.5;
-      this.setState({ trivia: response.results,
-        answers: answers.sort(() => Math.random() - n) });
     }
+    this.setState({ trivia: response.results });
+    const n = 0.5;
+    const answers = response.results.map((item) => (
+      [item.correct_answer, ...item.incorrect_answers].sort(() => Math.random() - n)
+    ));
+    this.setState({ answers });
   }
 
   handleClick = () => {
@@ -47,43 +63,48 @@ class Game extends React.Component {
   }
 
   render() {
-    const { trivia, index, answers, showAnswers } = this.state;
+    const { trivia, index, answers, showAnswers, timer, disabled } = this.state;
     return (
       <div data-testid="game-page-div">
         <Header />
-        {trivia.length > 0 && (
+        <p>{`Timer: ${timer}`}</p>
+        {trivia.length > 0 && answers.length > 0 && (
           <div>
             <p data-testid="question-category">{ trivia[index].category}</p>
             <p data-testid="question-text">{trivia[index].question}</p>
             <div data-testid="answer-options">
-              {answers.map(({ id, answer }) => {
-                if (id === 2 + 1) {
+              {answers[index].map((item, i) => {
+                if (item === trivia[index].correct_answer) {
                   return (
                     <button
                       style={ { border: showAnswers && '3px solid rgb(6, 240, 15)' } }
-                      key={ id }
+                      key={ item }
                       type="button"
                       data-testid="correct-answer"
                       onClick={ () => this.handleClick() }
+                      disabled={ disabled }
                     >
-                      {answer}
+                      {item}
                     </button>
                   );
-                } return (
+                }
+                return (
                   <button
                     style={ { border: showAnswers && '3px solid red' } }
-                    key={ id }
+                    key={ item }
                     type="button"
-                    data-testid={ `wrong-answer-${id}` }
+                    data-testid={ `wrong-answer-${i}` }
                     onClick={ () => this.handleClick() }
+                    disabled={ disabled }
                   >
-                    {answer}
+                    {item}
                   </button>
                 );
               })}
             </div>
           </div>
         )}
+
       </div>
     );
   }
