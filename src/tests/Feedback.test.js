516aae042923612effd22a2952178f55cac64e08
rafael <rafaelsm003@gmail.com>
2022-05-30 18:52:45 -0300

    test: requisito 17

---
+++ src/tests/Feedback.test.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { screen } from "@testing-library/react";
+import renderWithRouterAndRedux from "./helpers/renderWithRouterAndRedux";
+import App from "../App";
+import userEvent from "@testing-library/user-event";
+
+const initialState = {
+  player: {
+    name: 'teste',
+    gravatarEmail: 'teste@teste.com',
+    score: 50,
+    assertions: 4,
+  }
+}
+
+describe('Testes do Feedback', () => {
+  test('Testa a URL', () => {
+    const { history } = renderWithRouterAndRedux(<App />, initialState, "/feedback");
+    const { pathname } = history.location;
+    expect(pathname).toBe('/feedback');
+  });
+
+  test('Testa os textos de Feedback', () => {
+  const { history } = renderWithRouterAndRedux(<App />, initialState, "/feedback");
+  const assertionsText = screen.getByRole('heading', { level: 3, name: /Well Done!/i });
+  const score = screen.getByTestId('feedback-total-score');
+  expect(assertionsText).toBeInTheDocument();
+  expect(score).toHaveTextContent('50');
+  });
+
+  test('Testa o botão de jogar novamente', () => {
+    const { history } = renderWithRouterAndRedux(<App />, initialState, "/feedback");
+    const playAgainButton = screen.getByRole('button', { name: /play again/i });
+    expect(playAgainButton).toBeInTheDocument();
+    userEvent.click(playAgainButton);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  test('Testa o botão de ranking', () => {
+    localStorage.setItem('ranking', JSON.stringify([{
+      name: 'teste',
+      score: 50,
+      picture: 'https://www.gravatar.com/avatar/f2bc003d26af03264b9d48606bc6c53f',
+    }]));
+
+    const { history } = renderWithRouterAndRedux(<App />, initialState, "/feedback");
+    const rankingButton = screen.getByRole('button', { name: /ranking/i });
+    expect(rankingButton).toBeInTheDocument();
+    userEvent.click(rankingButton);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/ranking');
+  });
+});
--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -22,6 +22,7 @@ describe('Testa a página de Login', () => {
   });
 
   test('Testa a rota /games', async () => {
+    jest.setTimeout(15000);
     const { history } = renderWithRouterAndRedux(<App />)
     const nameInput = screen.getByTestId('input-player-name');
     const emailInput = screen.getByTestId('input-gravatar-email');
