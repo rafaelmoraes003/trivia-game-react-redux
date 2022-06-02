fcb408885c1c7e609c489bcb2c4d315e36a394b3
rafael <rafaelsm003@gmail.com>
2022-06-01 23:21:18 -0300

    passa por favor

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Header from '../components/Header';
-import pontuation from '../redux/actions/pontuation';
+import pontuation from '../redux/actions/pontuation'; //
 
 class Game extends React.Component {
   constructor() {
